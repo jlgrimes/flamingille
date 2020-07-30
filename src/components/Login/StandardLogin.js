6474977c55757cd28cf6b350e7406003b15dacb1
Jared Grimes <jaredgrimes@macbook-pro.lan>
2020-07-29 20:41:50 -0400

    native paper for login

--- src/components/Login/StandardLogin.js
+++ src/components/Login/StandardLogin.js
@@ -1,28 +1,45 @@
 import React, { useState } from 'react';
-import { Input, Text, SocialIcon } from 'react-native-elements';
+import { Alert } from 'react-native';
+import { TextInput, HelperText, Title } from 'react-native-paper';
+import { SocialIcon } from 'react-native-elements';
 import { Auth } from 'aws-amplify';
 
 const StandardLogin = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [usernamePasswordCorrect, setUsernamePasswordCorrect] = useState(true);
+
+  const logIn = async (username, password) => {
+    try {
+      const user = await Auth.signIn(username, password);
+    } catch (error) {
+      setUsernamePasswordCorrect(false);
+    }
+  };
+
   return (
     <>
-      <Input
-        placeholder="Email"
+      <Title>Login</Title>
+      <HelperText type="error" visible={!usernamePasswordCorrect}>
+        Email or password is incorrect!
+      </HelperText>
+      <TextInput
+        label="Email"
         onChange={(e) => setUsername(e.nativeEvent.text)}
+        error={!usernamePasswordCorrect}
       />
-      <Input
-        placeholder="Password"
-        type="password"
+      <TextInput
+        label="Password"
         secureTextEntry={true}
         onChange={(e) => setPassword(e.nativeEvent.text)}
+        error={!usernamePasswordCorrect}
       />
       <SocialIcon
         title="Log In"
         button
         type="medium"
         iconStyle={{ width: 0 }}
-        onPress={() => Auth.signIn(username, password)}
+        onPress={() => logIn(username, password)}
       />
     </>
   );
