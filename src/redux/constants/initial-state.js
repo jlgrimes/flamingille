e62b447e5290d9986af19fc6830b2759e11f8053
Jared Grimes <jaredlgrimes@hotmail.com>
2020-08-19 11:15:35 -0400

    added conversations initialState

--- src/redux/constants/initial-state.js
+++ src/redux/constants/initial-state.js
@@ -8,4 +8,7 @@ export const initialState = {
   // matched is what renders the "you got a match! modal"
   // if it's not null, it's the user of the person you matched with
   matched: null,
+
+  // the conversations that get rendered in Conversations page
+  conversations: [],
 };
