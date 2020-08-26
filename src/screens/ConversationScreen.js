323640ab71f869d612705798f223b6d8741d66fc
Jared Grimes <jaredlgrimes@hotmail.com>
2020-08-25 23:09:31 -0400

    giftedchat init

--- src/screens/ConversationScreen.js
+++ src/screens/ConversationScreen.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 
 import { connect } from 'react-redux';
 import { mapStateToProps, mapDispatchToProps } from '../redux/maps';
 
+import { GiftedChat } from 'react-native-gifted-chat';
+
 const ConversationScreen = ({ conversation }) => {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: 'Hello developer',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+        },
+      },
+    ]);
+  }, []);
+
+  const onSend = useCallback((messages = []) => {
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, messages),
+    );
+  }, []);
+
   return (
     <>
+      <GiftedChat
+        messages={messages}
+        onSend={(messages) => onSend(messages)}
+        user={{
+          _id: 1,
+        }}
+      />
     </>
   );
 };
